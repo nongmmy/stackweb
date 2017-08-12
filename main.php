8d36695c7d67997ac4514c97caf36746e9c8ba05
nongmmy <bulagorn4811@gmail.com>
2017-08-12 18:59:02 +0700

    edit get message

--- main.php
+++ main.php
@@ -7,18 +7,18 @@
 	if(isset($_POST['pop'])){
 		//check empty
 		if($thestack->isEmpty()){
-			header('Location: http://localhost/stack_web/index.php?msg=stack_is_empty');
+			header('Location: http://localhost/stack_web/index.php?msg=Stack is empty');
 		}
 		else{
 			$data = $thestack->pop();
-			header('Location: http://localhost/stack_web/index.php?msg=pop_method_success&data='.$data['data']);
+			header('Location: http://localhost/stack_web/index.php?msg=Pop successfully&data='.$data['data']);
 		}
 	}
 
 	if(isset($_POST['push'])){
 		$data = $_POST['data'];
 		$thestack->push($data);
-		header('Location: http://localhost/stack_web/index.php?msg=push_success');
+		header('Location: http://localhost/stack_web/index.php?msg=Push successfully');
 	}
 
 	if(isset($_POST['special'])){
@@ -27,11 +27,11 @@
 		$data = $_POST['data'];
 		//check index 
 		if($index > $thestack->size()){
-			header('Location: http://localhost/stack_web/index.php?msg=out_of_index');
+			header('Location: http://localhost/stack_web/index.php?msg=Out of index');
 		}
 		else{
 			$thestack->special($index,$data);
-			header('Location: http://localhost/stack_web/index.php?msg=special_method_success');
+			header('Location: http://localhost/stack_web/index.php?msg=special method successfully');
 		}
 	}
 
